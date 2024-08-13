bed0532a9337997b8a23e79b5fffb21d349c257d
teepeethree <thomas.w.potts@gmail.com>
2024-08-13 15:37:48 +0100

    Created outline of resume.ts

---
+++ src/pages/api/resume.ts
@@ -0,0 +1,24 @@
+/*
+
+Prompt the user to enter the password, if not authenticated, then break. Otherwise continue.
+
+Pull resume as JSON Object from database:
+
+- Name
+- Contact Details
+- Location
+- Education
+- Relevant job experience
+
+Retrieve template
+
+Use JSON object to populate template, including:
+
+- Links
+- Tagline
+
+Return populated template
+
+Download as PDF as "tpotts-resume-latest.pdf"
+
+*/
